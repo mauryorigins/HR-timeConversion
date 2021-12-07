561941d7467f799f1674f0a0a090df9f3bec72e7
Steven Barquet <mlmjm123@gmail.com>
2021-12-07 13:36:29 -0600

    challange1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+/* eslint-disable no-return-assign */
 // import inmutability from './inmutability';
-import bigTestCode from './bigTestCode';
+// import bigTestCode from './bigTestCode';
 
-function smallTestCode(){
-  console.log('Do something small');
+// Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+
+function simpleArraySum(ar: Array<number>): number {
+  let suma = 0;
+  ar.forEach((element) => suma += element);
+  return suma;
 }
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  smallTestCode();
-  bigTestCode();
+  const testArray = [1, 2, 3, 4, 10, 11];
+  const result = simpleArraySum(testArray);
+  console.log('Result: ', result);
 }
 
 main();
